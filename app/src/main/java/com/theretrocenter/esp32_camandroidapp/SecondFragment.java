78301ee84b5bd7190fd74e2b65e05da7e613c531
Jonathan Toledo Moya <pablotoledomoya@gmail.com>
2021-06-09 16:51:09 -0400

    base project

---
+++ app/src/main/java/com/theretrocenter/esp32_camandroidapp/SecondFragment.java
@@ -0,0 +1,61 @@
+package com.theretrocenter.esp32_camandroidapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
+public class SecondFragment extends Fragment {
+
+    private TextView mTextViewAngleRight;
+    private TextView mTextViewStrengthRight;
+    private TextView mTextViewCoordinateRight;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mTextViewAngleRight = (TextView) getView().findViewById(R.id.textView_angle_right);
+        mTextViewStrengthRight = (TextView) getView().findViewById(R.id.textView_strength_right);
+        mTextViewCoordinateRight = getView().findViewById(R.id.textView_coordinate_right);
+
+        final JoystickView joystickRight = (JoystickView) getView().findViewById(R.id.joystickView_right);
+        joystickRight.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onMove(int angle, int strength) {
+                mTextViewAngleRight.setText(angle + "°");
+                mTextViewStrengthRight.setText(strength + "%");
+                mTextViewCoordinateRight.setText(
+                        String.format("x%03d:y%03d",
+                                joystickRight.getNormalizedX(),
+                                joystickRight.getNormalizedY())
+                );
+            }
+        });
+
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+}
